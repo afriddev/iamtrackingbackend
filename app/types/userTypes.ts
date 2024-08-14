8169c92be2baf5efeda3b2a3d41752bebe2bf855
SHIAK AFRID <afridayan01@gmail.com>
2024-08-14 09:38:18 -0500

    added types

---
+++ app/types/userTypes.ts
@@ -0,0 +1,16 @@
+export type userType = {
+  emailId: string;
+  mobilenumber: number;
+  password: string;
+  fisrtName: string;
+  lastName?: string;
+  todayDate: number;
+  lastUpdatedDate: number;
+  monthLimitAmount: number;
+  todaySpends: number[];
+  balance: number;
+  totalSpend:number;
+  dailyLimit:number;
+  totalSaved:number
+  imageUrl:string
+};
