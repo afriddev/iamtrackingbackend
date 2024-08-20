38af1a6e969c0a00e720c75c3310aaf8a94c09b5
afriddev <afridayan01@gmail.com>
2024-08-20 12:52:59 +0530

    encoded password

--- app/api/createuser/route.ts
+++ app/api/createuser/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: Request) {
           firstName,
           lastName: lastName ?? "",
           emailId,
-          password,
+          password:btoa(password),
           todayDate: getTodayDate(),
           lastUpdatedDate: getTodayDate(),
           monthLimitAmount: 0,
