2ed3ce4087b71ebe46f061b2ebd184aa2ab26749
afriddev <afridayan01@gmail.com>
2024-08-26 14:39:05 +0530

    aa

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 
-export function GET() {
+export async function GET() {
   console.log("test");
   return NextResponse.json({
     Message: "asa",
   });
 }
+
+''
